b38715c8833017b63b96c1c704ff66b03de7fa3b
Prakash Rana <prakashrana10032@gmail.com>
2026-07-06 13:21:45 +0545

    React reminder folder update

---
+++ src/pages/AddPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Form from "../components/Form";
+
+const AddPage = () => {
+  return (
+    <section className="max-w-2xl mx-auto">
+      <h2 className="mb-4 text-xl font-bold text-gray-900 ">
+        Add a New Reminder
+      </h2>
+      <Form />
+    </section>
+  );
+};
+
+export default AddPage;
