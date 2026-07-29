72af8f9dd4352bf3236f8e2981383da6a5199b76
Prakash Rana <prakashrana10032@gmail.com>
2026-07-29 21:03:53 +0545

    fix:remember me

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -47,7 +47,7 @@ const LoginPage = () => {
         <form onSubmit={handleSubmit(submitForm)} className="space-y-4 md:space-y-6" action="#">
           <div>
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required {...register("email")}/>
+            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" required {...register("email")}/>
           </div>
           <div>
             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -56,7 +56,7 @@ const LoginPage = () => {
           <div className="flex items-center justify-between">
             <div className="flex items-start">
               <div className="flex items-center h-5">
-                <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
+                <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"/>
               </div>
               <div className="ml-3 text-sm">
                 <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
